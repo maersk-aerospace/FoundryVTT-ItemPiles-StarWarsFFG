95a6c45a65629f124fdd536b467563dc617c1af2
Maersk Aerospace <sirbayer@gmail.com>
2026-01-28 19:54:17 -0700

    add the basic pile config file

---
+++ modules/pilesconfig.js
@@ -0,0 +1,56 @@
+// fully just referencing the itempiles provided example, the DnD5e version
+// see https://github.com/fantasycalendar/FoundryVTT-ItemPilesDnD5e/blob/c8cde97962c5f1db4269a9a61cf9f4110b07381a/dist/module.js
+
+Hooks.once("item-piles-ready", async () => {
+    // we also use the deprecated itempiles swffg setting file as reference for our base configuration
+    const baseConfig = {
+        // type classes
+        "ACTOR_CLASS_TYPE": "character", // maintains best similarity to what you expect to do the most of
+        "ITEM_CLASS_LOOT_TYPE": "", // unnecessary
+        "ITEM_CLASS_WEAPON_TYPE": "", // unnecessary
+        "ITEM_CLASS_EQUIPMENT_TYPE": "", // unnecessary
+
+        // core attribute paths
+        "ITEM_QUANTITY_ATTRIBUTE": "system.quantity.value",
+        "ITEM_PRICE_ATTRIBUTE": "system.price.value",
+
+        // filters and similarities, filters out non-physical objects from piles and helps with stacking
+        "ITEM_FILTERS": [{
+            "path": "type",
+            "filters": "species,career,specialization,ability,criticaldamage,criticalinjury,talent,homesteadupgrade,signatureability,forcepower"
+        }],
+        "ITEM_SIMILARITIES": ["name", "type"],
+
+        // cash, duh
+        "CURRENCIES": [
+            {
+                "type": "attribute",
+                "name": "Credits",
+                "img": "systems/starwarsffg/images/mod-all.png",
+                "abbreviation": "{#}cr",
+                "data": {
+                    "path": "system.stats.credits.value"
+                },
+                "primary": true,
+                "exchangeRate": 1,
+                "index": 0,
+                "id": "system.stats.credits.value"
+            }
+        ]
+    }
+
+    // define versions object, extends the base config
+    // this lets us run over old configs with new versions
+    // see https://fantasycomputer.works/FoundryVTT-ItemPiles/#/contributing-to-item-piles?id=adding-system-support
+    const VERSIONS = {
+        "0.0.1": {
+            ...baseConfig,
+            "VERSION": "0.0.1"
+        }
+    }
+
+    // actually hook all the versions in now
+    for (const [version, data] of Object.entries(VERSIONS)) {
+        await game.itempiles.API.addSystemIntegration(data, version);
+    }
+});
